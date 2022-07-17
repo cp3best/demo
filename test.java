77e4da84b995f396b8e0654e44801aa962300ede
Chen Kuang <1071740786@qq.com>
2022-07-17 23:00:23 +0800

    test

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.util.*;;
+class test{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
+        System.out.print(a + a);
+    }
+}
